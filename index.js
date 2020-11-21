08b0f71d9454ae9ae20d562798a8327d14b968a1
Donsoner <jhernandezmontalvo@gmail.com>
2020-11-20 23:25:08 -0600

    server running

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//install express cors etc...
+const express= require('express');
+const app = express();
+let port= process.env.PORT || 3000;
+
+app.get("/", (req,res)=>{
+    res.send("HELLO WORD");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running in port: ${port}`);
+});
