1caf0ba32f7e542b53a8b6e5bbe38fad8d28a6ec
Donsoner <jhernandezmontalvo@gmail.com>
2020-11-23 17:43:36 -0600

    subir datos

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-require("dotenv").config();
+require('dotenv').config();
 const express= require('express');
 const app = express();
-let port= process.env.PORT || 3001;
-let mysql= require("mysql");
-
-//connection to database
-var connection= mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "login_controlaccess",
-    port: "3306"
-});
-connection.connect((err)=>{
-    if (err) {
-        throw err;
-    } else {
-        console.log("connected to database :D");
-    }
-});
-
+let port= process.env.TYPEORM_PORT || 3000;
+var mysql = require('mysql');
+//connection.end();
 //seend conexion to api request
 app.get("/", (req,res)=>{
-    res.send("HELLO WORD");
+    res.send("HELLO WORD ");
 });
 //running
 app.listen(port, ()=>{
     console.log(`Server running in port: ${port}`);
-});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    
+});  
+
+var connection = mysql.createConnection({
+   url: process.env.DATABASE_URL,
+   host: process.env.TYPEORM_HOST,
+   user: process.env.TYPEORM_USERNAME,
+   password: process.env.TYPEORM_PASSWORD,
+   database: process.env.TYPEORM_DATABASE
+
+})
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+})
+connection.end();
