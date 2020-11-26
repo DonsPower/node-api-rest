78962ef20fc7dc8e7ac9afc73a7be803d1c3fc6a
Donsoner <jhernandezmontalvo@gmail.com>
2020-11-26 01:00:05 -0600

    hambre

--- index.js
+++ index.js
@@ -12,34 +12,17 @@ app.listen(()=>{
     console.log(`Server running in port: ${process.env.TYPEORM_PORT}`);
     
 });  
+var connection = mysql.createPool({
+   url: process.env.DATABASE_URL,
+   type: process.env.TYPEORM_CONECTION,
+   host: process.env.TYPEORM_HOST,
+   user: process.env.TYPEORM_USERNAME,
+   password: process.env.TYPEORM_PASSWORD,
+   database: process.env.TYPEORM_DATABASE,
+  port : process.env.TYPEORM_PORT,               
+})
 
-
-function handleDisconnect() {
-   var connection = mysql.createConnection({
-      url: process.env.DATABASE_URL,
-      type: process.env.TYPEORM_CONECTION,
-      host: process.env.TYPEORM_HOST,
-      user: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      port : process.env.TYPEORM_PORT,               
-   })
-   connection.connect(function(error){
-      if(error){
-         throw error;
-      }else{
-         console.log('Conexion correcta.');
-      }
-   })                    
-   connection.on('error', function(err) {
-     console.log('db error', err);
-     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-       handleDisconnect();                         // lost due to either server restart, or a
-     } else {                                      // connnection idle timeout (the wait_timeout
-       throw err;                                  // server variable configures this)
-     }
-   });
-   connection.end();
- }
- 
- handleDisconnect();
+connection.on('error', function(err) {
+   console.log(err.code);
+ });
+connection.end();
