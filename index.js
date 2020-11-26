d1421c4aa257a46fd8309890eb06afd551d96677
Donsoner <jhernandezmontalvo@gmail.com>
2020-11-26 00:45:21 -0600

    deply heroku BD

--- index.js
+++ index.js
@@ -13,20 +13,33 @@ app.listen(()=>{
     
 });  
 
-var connection = mysql.createConnection({
-   url: process.env.DATABASE_URL,
-   type: process.env.TYPEORM_CONECTION,
-   host: process.env.TYPEORM_HOST,
-   user: process.env.TYPEORM_USERNAME,
-   password: process.env.TYPEORM_PASSWORD,
-   database: process.env.TYPEORM_DATABASE,
-   port : process.env.TYPEORM_PORT,               
-})
-connection.connect(function(error){
-   if(error){
-      throw error;
-   }else{
-      console.log('Conexion correcta.');
-   }
-})
-connection.end();
+
+function handleDisconnect() {
+   var connection = mysql.createConnection({
+      url: process.env.DATABASE_URL,
+      type: process.env.TYPEORM_CONECTION,
+      host: process.env.TYPEORM_HOST,
+      user: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+     // port : process.env.TYPEORM_PORT,               
+   })
+   connection.connect(function(error){
+      if(error){
+         throw error;
+      }else{
+         console.log('Conexion correcta.');
+      }
+   })                    
+   connection.on('error', function(err) {
+     console.log('db error', err);
+     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+       handleDisconnect();                         // lost due to either server restart, or a
+     } else {                                      // connnection idle timeout (the wait_timeout
+       throw err;                                  // server variable configures this)
+     }
+   });
+   connection.end();
+ }
+ 
+ handleDisconnect();
