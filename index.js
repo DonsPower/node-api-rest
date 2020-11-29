d31253ad3a1f77eefec962307e3f0e9491374a4e
Donsoner <jhernandezmontalvo@gmail.com>
2020-11-29 10:25:01 -0600

    add public file

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ require('dotenv').config();
 const express= require('express');
 const app = express();
 var mysql = require('mysql');
+var port = process.env.TYPEORM_PORT || 3000;
+app.use(express.static('./public'));
 //connection.end();
 //seend conexion to api request
 app.get("/", (req,res)=>{
     res.send("HELLO WORD ");
 });
 //running
-app.listen(()=>{
-    console.log(`Server running in port: ${process.env.TYPEORM_PORT}`);
+app.listen(port, ()=>{
+    console.log(`Server running in port: ${port}`);
     
 });  
-var connection = mysql.createPool({
+var connection = mysql.createConnection({
    url: process.env.DATABASE_URL,
-   type: process.env.TYPEORM_CONECTION,
    host: process.env.TYPEORM_HOST,
    user: process.env.TYPEORM_USERNAME,
    password: process.env.TYPEORM_PASSWORD,
-   database: process.env.TYPEORM_DATABASE,
-  port : process.env.TYPEORM_PORT,               
+   database: process.env.TYPEORM_DATABASE,               
 })
 
-connection.on('error', function(err) {
-   console.log(err.code);
- });
+connection.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('Conexion correcta.');
+   }
+})
 connection.end();
